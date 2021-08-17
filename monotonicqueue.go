c06feecdb65e282c51c211d47701d1cc3312edee
ZhengjunHUO <firelouiszj@hotmail.com>
2021-08-17 10:45:50 +0200

    add monotonic queue struct

---
+++ monotonicqueue.go
@@ -0,0 +1,48 @@
+package godtype
+
+import (
+	"reflect"
+)
+
+type MonotonicQueue struct {
+	deque	*Deque
+}
+
+func NewMonotonicQueue() *MonotonicQueue {
+	return &MonotonicQueue{NewDeque()}
+}
+
+func (mq *MonotonicQueue) Pop(elem interface{}) {
+	if elem == mq.deque.PeekFirst() {
+		mq.deque.PopFirst()
+	}
+}
+
+func (mq *MonotonicQueue) Push(elem interface{}) {
+	switch v := reflect.ValueOf(elem); v.Kind() {
+	case reflect.Int:
+		for ! mq.deque.IsEmpty() && mq.deque.PeekLast().(int) < int(v.Int()) {
+			mq.deque.PopLast()
+		}
+	case reflect.String:
+		for ! mq.deque.IsEmpty() && mq.deque.PeekLast().(string) < v.String() {
+			mq.deque.PopLast()
+		}
+	default:
+		return
+	}
+
+	mq.deque.PushLast(elem)
+}
+
+func (mq *MonotonicQueue) Max() interface{} {
+	return mq.deque.PeekFirst()
+}
+
+func (mq *MonotonicQueue) IsEmpty() bool {
+	return mq.deque.IsEmpty()
+}
+
+func (mq *MonotonicQueue) Size() int {
+	return mq.deque.Size()
+}
